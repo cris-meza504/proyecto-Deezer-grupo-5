77bc7a9a76a9269180b77f55418185245661db96
Jerh21 <hamjosue27@gmail.com>
2024-12-09 20:15:03 -0600

    RegistroFuncionalConBackend

--- src/components/logincomps/LoginComponent.jsx
+++ src/components/logincomps/LoginComponent.jsx
@@ -1,56 +1,74 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import countries from "../../js/countries";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 import lbLogo from "../../img/lb.png";
-import styles from "../../css/Login.module.css";
-import { useNavigate } from "react-router-dom";
-import React from "react";
+import styles from "../../css/Login.module.css"; // Asegúrate de tener tu CSS aquí
 
 const LoginComponent = () => {
-  const navigate = useNavigate(); // Hook para redirigir
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(""); // Para mostrar mensajes de error
+  const navigate = useNavigate();
 
-  const handleLogin = () => {
-    // Redirigir a la página principal (ajusta la ruta según corresponda)
-    navigate("/app/HomePage");
-  };
 
-  const handleClick = () => {
-    // Redirigir a la página principal (ajusta la ruta según corresponda)
-    navigate("/register");
+  const handleLogin = async (e) => {
+    e.preventDefault(); // Prevenir recarga de página al enviar el formulario
+
+    try {
+      // Realizar la solicitud POST con Axios
+      const response = await axios.post("http://127.0.0.1:8000/login", {
+        correo: email,
+        contrasenna: password,
+      });
+
+      if (response.status === 200) {
+        // Si las credenciales son correctas, redirigir a la página principal
+        navigate("/app/HomePage");
+      }
+    } catch (error) {
+      if (error.response) {
+        // Si hay una respuesta con un error de la API (404, 401, etc.)
+        setError(error.response.data.detail);
+      } else {
+        // Si hay un error de red o algo inesperado
+        setError("No se pudo conectar al servidor. Intenta más tarde.");
+      }
+    }
   };
 
   return (
-    <body className={styles.loginPage}>
+    <div className={styles.loginPage}>
       <div className={styles.header}>
         <img src={lbLogo} alt="Deezer Logo" className={styles.logo} />
       </div>
       <div className={styles.loginContainer}>
         <div className={styles.loginCard}>
-          <h1 className={styles.title}>Inciar Sección</h1>
-          <form action="#" method="POST">
+          <h1 className={styles.title}>Iniciar Sesión</h1>
+          <form onSubmit={handleLogin}>
             <div className={styles.inputGroup}>
-              <label htmlFor="country">países</label>
-              <select id="country" name="country" required>
-                <option value="">Honduras</option> {}
-                {countries.map((country, index) => (
-                  <option key={index} value={country}>
-                    {country}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className={styles.inputGroup}>
-              <label htmlFor="correo">Correo electrónico</label>
-              <input type="text" id="correo" name="correo" required />
+              <label htmlFor="email">Correo Electrónico</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
             </div>
             <div className={styles.inputGroup}>
               <label htmlFor="password">Contraseña</label>
-              <input type="password" id="password" name="password" required />
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
             </div>
-            <button
-              type="submit"
-              onClick={handleLogin}
-              className={styles.loginButton}
-            >
+            {error && <p className={styles.error}>{error}</p>} {/* Mostrar mensaje de error */}
+            <button type="submit" className={styles.loginButton}>
               Acceso
             </button>
           </form>
@@ -59,23 +77,17 @@ const LoginComponent = () => {
       <p className={styles.signupLink2}>
         ¿Aún no estás registrado en Deezer?{" "}
         <span className={styles.registerButton}>
-          <a href="#" onClick={handleClick}>
-            Inscribirse
-          </a>
+          <Link to="/register">Inscribirse</Link>
         </span>
       </p>
       <p className={styles.signupLink3}>
         Este sitio está protegido por reCAPTCHA. Se aplican{" "}
-        <a href="https://policies.google.com/privacy">
-          la Política de privacidad
-        </a>{" "}
+        <a href="https://policies.google.com/privacy">la Política de privacidad</a>{" "}
         y{" "}
-        <a href="https://policies.google.com/privacy">
-          los Términos de servicio
-        </a>{" "}
+        <a href="https://policies.google.com/terms">los Términos de servicio</a>{" "}
         de Google.
       </p>
-    </body>
+    </div>
   );
 };
 
--- src/components/logincomps/RegisterComponent.jsx
+++ src/components/logincomps/RegisterComponent.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
+import axios from "axios"; // Importa axios
 import lbLogo from "../../img/lb.png";
+import { useNavigate} from "react-router-dom";
 import styles from "../../css/Register.module.css";
 
 const RegisterComponent = () => {
   const [currentSection, setCurrentSection] = useState(1);
+  const [email, setEmail] = useState(""); // Para almacenar el correo
+  const [password, setPassword] = useState(""); // Para almacenar la contraseña
+  const [name, setName] = useState(""); // Para almacenar el nombre
+  const [age, setAge] = useState(""); // Para almacenar la edad
+  const [gender, setGender] = useState(""); // Para almacenar el género
+  const [error, setError] = useState(""); // Para mostrar mensajes de error
+  const navigate = useNavigate();
 
   const nextSection = () => {
     setCurrentSection((prevSection) => prevSection + 1);
@@ -17,15 +26,79 @@ const RegisterComponent = () => {
     return `Paso ${currentSection} de 3`;
   };
 
+  const checkEmailAvailability = async () => {
+    try {
+      // Verificar si el correo está registrado en el backend
+      const response = await axios.get(`http://127.0.0.1:8000/users/email/${email}`);
+      if (response.data.exists) {
+        // Si el correo ya está registrado
+        setError("Este correo ya está registrado.");
+      } else {
+        // Si el correo no está registrado
+        setError("");
+        nextSection(); // Pasar a la siguiente sección
+      }
+    } catch (error) {
+      setError("Error al verificar el correo.");
+    }
+  };
+
+  const handleSubmitRegistration = async (e) => {
+    e.preventDefault();
+  
+    // Mapear los valores de género a números
+    const genderMap = {
+      "female": 2,   // Femenino
+      "male": 1,     // Masculino
+      "other": 3,    // No binario
+      "otros": 4,    // Otros
+    };
+  
+    const genderValue = genderMap[gender] || 0; // Asignar 0 si no hay un valor válido
+  
+    try {
+      // Obtener el último código de usuario
+      const lastUserResponse = await axios.get("http://127.0.0.1:8000/users/last");
+      const lastUser = lastUserResponse.data;
+  
+      const newUserId = lastUser ? lastUser.codigo_usuario + 1 : 1; // Obtener el siguiente ID
+  
+      // Obtener la fecha actual en formato yyyy-mm-dd
+      const currentDate = new Date().toISOString().split('T')[0]; // Formato: "YYYY-MM-DD"
+  
+      // Realizar el registro del nuevo usuario
+      const response = await axios.post("http://127.0.0.1:8000/users", {
+        codigo_usuario: newUserId,
+        codigo_suscripcion_actual: 1,  // Como mencionaste, el código de suscripción será "1"
+        correo: email,
+        contrasenna: password,
+        nombre_usuario: name,
+        edad: age,
+        codigo_identidad: genderValue, // Enviar un valor numérico
+        fecha_registro: currentDate, // Enviar la fecha actual
+        url_foto_perfil: "", // Si tienes un campo opcional, por ejemplo, foto de perfil
+      });
+
+      
+      if (response.status === 200) {
+        // Si las credenciales son correctas, redirigir a la página principal
+        alert("Usuario registrado con éxito.");
+        navigate("/");
+      }
+
+      // Aquí podrías redirigir a otra página o mostrar un mensaje de éxito
+    } catch (error) {
+      console.error("Error al registrar el usuario", error);
+      setError("Error al registrar el usuario.");
+    }
+  };
+
   return (
     <div className={styles.wrapper}>
-      {" "}
-      {/* Agrega un contenedor principal */}
       <div className={styles.header}>
         <img src={lbLogo} alt="Deezer Logo" className={styles.logo} />
       </div>
       <div className={styles.loginContainer}>
-        {/* Encabezado */}
         <div
           style={{
             display: "flex",
@@ -52,19 +125,12 @@ const RegisterComponent = () => {
             {getProgressText()}
           </span>
         </div>
-        {/* Sección 1 */}
+
+        {/* Sección 1: Verificar Correo */}
         {currentSection === 1 && (
-          <div
-            id="section-1"
-            className={`${styles.loginCard} ${styles.section}`}
-            style={{
-              width: "481px",
-            }}
-          >
-            <h1 className={styles.title}>
-              Introduzca su dirección de correo electrónico
-            </h1>
-            <form>
+          <div id="section-1" className={`${styles.loginCard} ${styles.section}`} style={{ width: "481px" }}>
+            <h1 className={styles.title}>Introduce tu correo electrónico</h1>
+            <form onSubmit={checkEmailAvailability}>
               <div className={styles.inputGroup}>
                 <label htmlFor="email">Correo electrónico</label>
                 <input
@@ -73,39 +139,25 @@ const RegisterComponent = () => {
                   name="email"
                   placeholder="Email address"
                   required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
+              {error && <p style={{ color: "red" }}>{error}</p>}
               <button
                 type="button"
                 className={styles.loginButton}
-                onClick={nextSection}
+                onClick={checkEmailAvailability}
               >
                 Continuar
               </button>
             </form>
-            <p className={styles.signupLink3}>
-              Este sitio está protegido por reCAPTCHA. Se aplican{" "}
-              <a href="https://policies.google.com/privacy">
-                la Política de privacidad
-              </a>{" "}
-              y{" "}
-              <a href="https://policies.google.com/privacy">
-                los Términos de servicio
-              </a>{" "}
-              de Google.
-            </p>
           </div>
         )}
 
-        {/* Sección 2 */}
+        {/* Sección 2: Crear Contraseña */}
         {currentSection === 2 && (
-          <div
-            id="section-2"
-            className={`${styles.loginCard} ${styles.section}`}
-            style={{
-              width: "481px",
-            }}
-          >
+          <div id="section-2" className={`${styles.loginCard} ${styles.section}`} style={{ width: "481px" }}>
             <h1 className={styles.title}>Crear una Contraseña</h1>
             <form>
               <div className={styles.inputGroup}>
@@ -116,6 +168,8 @@ const RegisterComponent = () => {
                   name="password"
                   placeholder="Crea una contraseña"
                   required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
               <div className={styles.buttonGroup}>
@@ -131,17 +185,11 @@ const RegisterComponent = () => {
           </div>
         )}
 
-        {/* Sección 3 */}
+        {/* Sección 3: Datos Personales */}
         {currentSection === 3 && (
-          <div
-            id="section-3"
-            className={`${styles.loginCard} ${styles.section}`}
-            style={{
-              width: "481px",
-            }}
-          >
+          <div id="section-3" className={`${styles.loginCard} ${styles.section}`} style={{ width: "481px" }}>
             <h1 className={styles.title}>Introduzca sus datos personales</h1>
-            <form>
+            <form onSubmit={handleSubmitRegistration}>
               <div className={styles.inputGroup}>
                 <label htmlFor="name">Nombre de usuario</label>
                 <input
@@ -150,6 +198,8 @@ const RegisterComponent = () => {
                   name="name"
                   placeholder="Ingresa tu nombre completo"
                   required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                 />
               </div>
               <div className={styles.inputGroup}>
@@ -160,11 +210,19 @@ const RegisterComponent = () => {
                   name="age"
                   placeholder="Ingresa tu edad"
                   required
+                  value={age}
+                  onChange={(e) => setAge(e.target.value)}
                 />
               </div>
               <div className={styles.inputGroup}>
                 <label htmlFor="gender">Identidad</label>
-                <select id="gender" name="gender" required>
+                <select
+                  id="gender"
+                  name="gender"
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                  required
+                >
                   <option value="female">Identidad</option>
                   <option value="female">Femenino</option>
                   <option value="male">Masculino</option>
@@ -179,16 +237,9 @@ const RegisterComponent = () => {
               </div>
             </form>
             <p className={styles.signupLink3}>
-              Al hacer clic en &quot;Registrate gratis&quot;, aceptas crear una
-              cuenta y aceptas los{" "}
-              <a href="https://www.deezer.com/legal/cgu">
-                Términos y Condiciones de Uso
-              </a>{" "}
-              y la{" "}
-              <a href="https://www.deezer.com/legal/personal-datas">
-                Política de Privacidad
-              </a>
-              .
+              Al hacer clic en &quot;Registrate gratis&quot;, aceptas crear una cuenta y aceptas los{" "}
+              <a href="https://www.deezer.com/legal/cgu">Términos y Condiciones de Uso</a> y la{" "}
+              <a href="https://www.deezer.com/legal/personal-datas">Política de Privacidad</a>.
             </p>
           </div>
         )}
